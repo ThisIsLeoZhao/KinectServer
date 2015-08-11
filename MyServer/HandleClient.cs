1d0917818363c74583c0ad3a5ddbfaa749adef74
Guoliang <guolaing.zhao14@imperial.ac.uk>
2015-08-11 10:25:21 +0100

    server1.0

---
+++ MyServer/HandleClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MyServer
+{
+    public class HandleClient
+    {
+        private TcpClient client;
+        private static Hashtable clientTable = new Hashtable();
+
+        public HandleClient(TcpClient tcpClient)
+        {
+            this.client = tcpClient;
+            clientTable.Add(client, Server.clientID);
+        }
+
+        public void doChat(Object threadContext)
+        {
+            while (true)
+            {
+                try
+                {
+                    NetworkStream nwStream = client.GetStream();
+
+                    StreamWriter writer = new StreamWriter(nwStream, Encoding.ASCII) { AutoFlush = true };
+                    StreamReader reader = new StreamReader(nwStream, Encoding.ASCII);
+
+                    string inputLine = reader.ReadLine();
+
+                    Console.WriteLine(inputLine);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex);
+                    client.Close();
+                    Console.WriteLine("Client {0} exit.", clientTable[client]);
+                    int a;
+                    int b;
+                    ThreadPool.GetAvailableThreads(out a, out b);
+                    Console.WriteLine("{0}, {1}", a, b);
+                    return;
+                }
+
+            }
+
+
+
+        }
+    }
+}
---
+++ MyServer/Server.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using Newtonsoft.Json;
+using SingleKinect.EngagementManager;
+
+namespace MyServer
+{
+    public class Account
+    {
+        public bool hasReceived;
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class Server
+    {
+        private static TcpListener listener;
+        private static TcpClient client;
+        internal static int clientID = 0;
+
+        public static void start()
+        {
+            IPAddress localAdd = IPAddress.Parse("127.0.0.1");
+            listener = new TcpListener(localAdd, 5000);
+            Console.WriteLine("Listening...");
+            listener.Start();
+            
+            while (true)
+            {
+                //---incoming client connected---
+                client = listener.AcceptTcpClient();
+                
+                HandleClient clientHandler = new HandleClient(client);
+                ThreadPool.QueueUserWorkItem(clientHandler.doChat, clientID);
+
+                Console.WriteLine("client {0} on board", clientID);
+                clientID++;
+            }
+            
+            close();
+        }
+
+        private static void close()
+        {
+            
+            listener.Stop();
+        }
+    }
+
+    
+}
